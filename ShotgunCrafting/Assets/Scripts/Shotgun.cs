e6eefc6995100534e3c9c9b7d85ad785681e0574
Joel Nyonga <joel.nyonga@mymail.champlain.edu>
2025-01-29 19:57:47 -0500

    Still working on bullet spread

--- ShotgunCrafting/Assets/Scripts/Shotgun.cs
+++ ShotgunCrafting/Assets/Scripts/Shotgun.cs
@@ -5,6 +5,7 @@ using UnityEngine.Windows;
 
 public class Shotgun : MonoBehaviour
 {
+
     int maxAmmo;
     [SerializeField] GameObject muzzle;
     [SerializeField] int ammo;
@@ -16,12 +17,14 @@ public class Shotgun : MonoBehaviour
 
     private void Start()
     {
+     
+
         ammo = 6;
         maxAmmo = 6;
 
         ray1 = new Ray(muzzle.transform.position, muzzle.transform.forward);
-        ray2 = new Ray(muzzle.transform.position, Quaternion.Euler(0, 15, 0) * muzzle.transform.forward);
-        ray3 = new Ray(muzzle.transform.position, Quaternion.Euler(0, -15, 0) * muzzle.transform.forward);
+        ray2 = new Ray(muzzle.transform.position, Quaternion.Euler(-10, 5, 0) * muzzle.transform.forward);
+        ray3 = new Ray(muzzle.transform.position, Quaternion.Euler(-10, -5, 0) * muzzle.transform.forward);
     }
     private void Update()
     {
@@ -33,8 +36,8 @@ public class Shotgun : MonoBehaviour
         {
             Shoot();
             Debug.DrawRay(muzzle.transform.position, muzzle.transform.forward * maxDistance, Color.red, 2f);
-            Debug.DrawRay(muzzle.transform.position, Quaternion.Euler(0, 15, 0) * muzzle.transform.forward * maxDistance, Color.red, 2f);
-            Debug.DrawRay(muzzle.transform.position, Quaternion.Euler(0, -15, 0) * muzzle.transform.forward * maxDistance, Color.red, 2f);
+            Debug.DrawRay(muzzle.transform.position, Quaternion.Euler(-10, 5, 0) * muzzle.transform.forward * maxDistance, Color.red, 2f);
+            Debug.DrawRay(muzzle.transform.position, Quaternion.Euler(-10, -5, 0) * muzzle.transform.forward * maxDistance, Color.red, 2f);
         }
         //stopped performing context
         else
@@ -48,20 +51,23 @@ public class Shotgun : MonoBehaviour
         if (ammo > 0)
         {
             Debug.Log("Shot");
+            --ammo;
             if (Physics.Raycast(ray1, out RaycastHit hit1, maxDistance))
             {
                 Debug.Log(hit1.collider.gameObject.name + " was hit!");
-
+               
             }
 
             if (Physics.Raycast(ray2, out RaycastHit hit2, maxDistance))
             {
                 Debug.Log(hit2.collider.gameObject.name + " was hit!");
+               
             }
 
             if (Physics.Raycast(ray3, out RaycastHit hit3, maxDistance))
             {
                 Debug.Log(hit3.collider.gameObject.name + " was hit!");
+                
             }
         }
     }
